925d9a64b1bd33e8622b18a4a6fd437db8929722
Santhosh796373 <sandysanthosh785@gmail.com>
2025-07-20 11:30:42 +0530

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 Hello from SimpleNodeApp deployed via Lambdaaa + nn8nn!');
+  res.send('🚀 Hello from SimpleNodeApp deployed via Lambdaaaaa + nn8nn!');
 });
 
 app.listen(PORT, () => {
